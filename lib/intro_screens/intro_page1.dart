3a4c4ec675beb535d463edad347a02cb069e7cfb
FurkanPolatt <polat_2508@hotmail.com>
2022-05-05 00:50:42 +0300

    İntro 1 sayfası Tamamlandı

--- lib/intro_screens/intro_page1.dart
+++ lib/intro_screens/intro_page1.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroPage1 extends StatelessWidget {
   const IntroPage1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(child: Text('Intro 1')),
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[200],
+      body: SafeArea(
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SizedBox(height: 30),
+            Lottie.network(
+                'https://assets3.lottiefiles.com/packages/lf20_bfgchzaf.json'),
+            SizedBox(height: 50),
+            Text(
+              'Müziğin derinlerine ineceğiniz Vivace dünyasına hoşgeldiniz!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
