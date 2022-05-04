cbb22f9f89c094716a574f5d032d6fbbd325c08f
hazaltaneri <hazaltaneri@gmail.com>
2022-05-05 00:03:44 +0300

    Intro page 1 edited

--- lib/intro_screens/intro_page1.dart
+++ lib/intro_screens/intro_page1.dart
@@ -17,7 +17,7 @@ class IntroPage1 extends StatelessWidget {
                   Lottie.network(
                         'https://assets3.lottiefiles.com/packages/lf20_bfgchzaf.json'),
                   SizedBox(height: 50),
-                  Text('Müziğin derinlerine ineceğiniz Vivance dünyasına hoşgeldiniz!',
+                  Text('Müziğin derinlerine ineceğiniz Vivace dünyasına hoşgeldiniz!',
                     textAlign: TextAlign.center,
                     style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
                     ),
