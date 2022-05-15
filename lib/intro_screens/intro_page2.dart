20677eac4b86332565a297117a8aefa58e3af10f
hazaltaneri <hazaltaneri@gmail.com>
2022-05-15 16:22:11 +0300

    Intro sayfası 2 tamamlandı (son hali)

--- lib/intro_screens/intro_page2.dart
+++ lib/intro_screens/intro_page2.dart
@@ -13,100 +13,112 @@ class IntroPage2 extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              SizedBox(height: 50,),
-              Row(
-                children: [
-                  Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('assets/screenshot_images/anasayfa.png'),
-                  ),
-                  Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('assets/screenshot_images/enstrümanlar_anasayfası.png'),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                    width: 200,
-                    child: AnimatedTextKit(
-                      totalRepeatCount: 1,
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                            'Giriş yapar yapmaz sizi ana ekranımız karşılar',
-                            textAlign: TextAlign.center,
-                            speed: Duration(milliseconds: 50),
-                            textStyle:
-                            GoogleFonts.robotoMono(fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 200,
-                    child: AnimatedTextKit(
-                      totalRepeatCount: 1,
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                            'Öncelikle bir enstrüman başlığı seçebilirsiniz',
-                            textAlign: TextAlign.center,
-                            speed: Duration(milliseconds: 50),
-                            textStyle:
-                            GoogleFonts.robotoMono(fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
               SizedBox(height: 30,),
-              Row(
-                children: [
-                  Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('assets/screenshot_images/enstrüman_örneksayfa.png'),
-                  ),
-                  Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('assets/screenshot_images/kendini_testet.png'),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                    width: 200,
-                    child: AnimatedTextKit(
-                      totalRepeatCount: 1,
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                            'Seçtiğiniz enstrüman için detaylı bilgileri bu sayfalarda bulabilirsiniz',
-                            textAlign: TextAlign.center,
-                            speed: Duration(milliseconds: 50),
-                            textStyle:
-                            GoogleFonts.robotoMono(fontWeight: FontWeight.bold)),
-                      ],
+              Expanded(
+                flex: 3,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Image(
+                        image: AssetImage('assets/app_images/anasayfa.png'),
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: 200,
-                    child: AnimatedTextKit(
-                      totalRepeatCount: 1,
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                            'Kendini test et bölümünde müzik bilginizi test edebilirsiniz',
-                            textAlign: TextAlign.center,
-                            speed: Duration(milliseconds: 50),
-                            textStyle:
-                            GoogleFonts.robotoMono(fontWeight: FontWeight.bold)),
-                      ],
+                    Expanded(
+                      child: Image(
+                        image: AssetImage('assets/app_images/enstrümanlar_anasayfası.png'),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 200,
+                      child: AnimatedTextKit(
+                        totalRepeatCount: 1,
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                              'Giriş yapar yapmaz sizi ana ekranımız karşılar',
+                              textAlign: TextAlign.center,
+                              speed: Duration(milliseconds: 50),
+                              textStyle:
+                              GoogleFonts.robotoMono(fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      child: AnimatedTextKit(
+                        totalRepeatCount: 1,
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                              'Öncelikle bir enstrüman başlığı seçebilirsiniz',
+                              textAlign: TextAlign.center,
+                              speed: Duration(milliseconds: 50),
+                              textStyle:
+                              GoogleFonts.robotoMono(fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Image(
+                        image: AssetImage('assets/app_images/enstrüman_örneksayfa.png'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Image(
+                        image: AssetImage('assets/app_images/kendini_testet.png'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 200,
+                      child: AnimatedTextKit(
+                        totalRepeatCount: 1,
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                              'Seçtiğiniz enstrüman için detaylı bilgileri bu sayfalarda bulabilirsiniz',
+                              textAlign: TextAlign.center,
+                              speed: Duration(milliseconds: 50),
+                              textStyle:
+                              GoogleFonts.robotoMono(fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      child: AnimatedTextKit(
+                        totalRepeatCount: 1,
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                              'Kendini test et bölümünde müzik bilginizi test edebilirsiniz',
+                              textAlign: TextAlign.center,
+                              speed: Duration(milliseconds: 50),
+                              textStyle:
+                              GoogleFonts.robotoMono(fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 75,),
             ],
           ),
         ),
