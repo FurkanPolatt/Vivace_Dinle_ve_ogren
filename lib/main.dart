0dc38fda9bc70339426bf868ed1f0c0ba3566488
Onur Arık <62481825+arikonur@users.noreply.github.com>
2022-05-16 22:45:59 +0300

    add intro loading page sharedprefences

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:musicedu_app/loading_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() async {
+import 'home_page.dart';
+import 'loading_screen.dart';
+import 'login_page/main_page.dart';
+
+dynamic initScreen;
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefences = await SharedPreferences.getInstance();
+  initScreen = await prefences.getInt('initScreen');
+  await prefences.setInt('initScreen', 1);
   await Firebase.initializeApp();
   runApp(const Vivace());
 }
@@ -14,6 +24,11 @@ class Vivace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, home: const LoadingPage());
+      initialRoute: initScreen == 0 || initScreen == null ? 'onBoard' : 'home',
+      routes: {
+        'onBoard': (context) => LoadingPage(),
+        'home': (context) => MainPage(),
+      },
+    );
   }
 }
